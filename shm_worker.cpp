b05a22b1f9ae118b40a80b0a4cec1efb4e3fa5b4
Zhen Zhang <zarzen@Zhens-iMac.local>
2020-03-16 19:45:11 -0400

    extract worker mem

--- shm_worker.cpp
+++ shm_worker.cpp
@@ -44,10 +44,8 @@ INSTR_T instr_map(int idx) {
   };
 };
 
-class SHMWorker {
-  std::string name;
-  EFAEndpoint* efa_ep;
-
+class WorkerMemory {
+ public:
   // 12 + 64 Bytes: 8 bytes for timestamp;
   // 4 bytes for instr-code; 64 bytes for instr data
   int instr_size = 76;
@@ -69,24 +67,16 @@ class SHMWorker {
   void* efa_add_ptr;
   void* status_ptr;  // 1: idle; 2: working;
 
- public:
-  SHMWorker(std::string name) {
-    efa_ep = new EFAEndpoint((this->name + "-efa-ep"));
-    //
-    shm_instr = std::string(name + "-instr-mem");
-    shm_cntr = std::string(name + "-cntr-mem");
-    shm_data_buf = std::string(name + "-data-buf-mem");
-    shm_efa_addr = std::string(name + "-efa-addr-mem");
-    shm_w_status = std::string(name + "-worker-status-mem");
-
-    this->name = name;
-    this->create_shm();
-    this->set_local_efa_addr(efa_ep);
-    // init worker status
-    *(int*)status_ptr = 1;
+  WorkerMemory(std::string prefix, bool truncate) {
+    shm_instr = std::string(prefix + "-instr-mem");
+    shm_cntr = std::string(prefix + "-cntr-mem");
+    shm_data_buf = std::string(prefix + "-data-buf-mem");
+    shm_efa_addr = std::string(prefix + "-efa-addr-mem");
+    shm_w_status = std::string(prefix + "-worker-status-mem");
+    this->create_shm(truncate);
   };
 
-  void create_shm() {
+  void create_shm(bool truncate) {
     int instr_fd = shm_open(shm_instr.c_str(), O_CREAT | O_RDWR, 0666);
     int cntr_fd = shm_open(shm_cntr.c_str(), O_CREAT | O_RDWR, 0666);
     int data_buf_fd = shm_open(shm_data_buf.c_str(), O_CREAT | O_RDWR, 0666);
@@ -95,16 +85,17 @@ class SHMWorker {
         shm_open(shm_w_status.c_str(), O_CREAT | O_RDWR, 0666);
 
     // truncate memory size
-    check_err((ftruncate(instr_fd, instr_size) < 0),
-              "ftruncate instr_fd err\n");
-    check_err((ftruncate(cntr_fd, cntr_size) < 0), "ftruncate cntr_fd err\n");
-    check_err((ftruncate(data_buf_fd, data_buf_size) < 0),
-              "ftruncate data_buf_fd err\n");
-    check_err((ftruncate(efa_add_fd, efa_addr_size) < 0),
-              "ftruncate efa_add_fd err\n");
-    check_err((ftruncate(worker_status_fd, status_size) < 0),
-              "ftruncate worker_status_fd err\n");
-
+    if (truncate) {
+      check_err((ftruncate(instr_fd, instr_size) < 0),
+                "ftruncate instr_fd err\n");
+      check_err((ftruncate(cntr_fd, cntr_size) < 0), "ftruncate cntr_fd err\n");
+      check_err((ftruncate(data_buf_fd, data_buf_size) < 0),
+                "ftruncate data_buf_fd err\n");
+      check_err((ftruncate(efa_add_fd, efa_addr_size) < 0),
+                "ftruncate efa_add_fd err\n");
+      check_err((ftruncate(worker_status_fd, status_size) < 0),
+                "ftruncate worker_status_fd err\n");
+    }
     instr_ptr =
         mmap(0, instr_size, PROT_READ | PROT_WRITE, MAP_SHARED, instr_fd, 0);
     cntr_ptr =
@@ -117,21 +108,53 @@ class SHMWorker {
                       worker_status_fd, 0);
   };
 
+  ~WorkerMemory() {
+    // un map
+    munmap(instr_ptr, instr_size);
+    munmap(cntr_ptr, cntr_size);
+    munmap(data_buf_ptr, data_buf_size);
+    munmap(efa_add_ptr, efa_addr_size);
+    munmap(status_ptr, status_size);
+
+    // un link
+    shm_unlink(shm_instr.c_str());
+    shm_unlink(shm_cntr.c_str());
+    shm_unlink(shm_data_buf.c_str());
+    shm_unlink(shm_efa_addr.c_str());
+    shm_unlink(shm_w_status.c_str());
+  };
+};
+
+class SHMWorker {
+  std::string name;
+  EFAEndpoint* efa_ep;
+  WorkerMemory* mem;
+
+ public:
+  SHMWorker(std::string name) {
+    efa_ep = new EFAEndpoint((this->name + "-efa-ep"));
+    mem = new WorkerMemory(name, true);
+    this->name = name;
+    this->set_local_efa_addr(efa_ep);
+    // init worker status
+    *(int*)(mem->status_ptr) = 1;
+  };
+
   Instruction* read_instr() {
     // read first 8 bytes for time stamp
-    double ts = *((double*)instr_ptr);
+    double ts = *((double*)mem->instr_ptr);
     if (ts == 0) {
       return NULL;
     } else {
       Instruction* i = new Instruction();
       i->timestamp = ts;
       // start from 9th byte to 12th stand for instr
-      INSTR_T i_type = instr_map(*((int*)((char*)instr_ptr + 8)));
+      INSTR_T i_type = instr_map(*((int*)((char*)mem->instr_ptr + 8)));
       i->type = i_type;
-      i->data = ((char*)instr_ptr) + 12;
+      i->data = ((char*)mem->instr_ptr) + 12;
 
       // wipe first 12 bit to inidicate message already read
-      std::fill_n(instr_ptr, 12, 0);
+      std::fill_n(mem->instr_ptr, 12, 0);
       return i;
     }
   };
@@ -139,11 +162,11 @@ class SHMWorker {
   void set_local_efa_addr(EFAEndpoint* efa) {
     char local_ep_addrs[64] = {0};
     char readable[64] = {0};
-    efa->get_name(local_ep_addrs, efa_addr_size);
-    size_t len = efa_addr_size;
+    efa->get_name(local_ep_addrs, mem->efa_addr_size);
+    size_t len = mem->efa_addr_size;
     fi_av_straddr(efa->av, local_ep_addrs, readable, &len);
     std::cout << "Local ep addresses: \n" << readable << "\n";
-    std::memcpy(efa_add_ptr, local_ep_addrs, efa_addr_size);
+    std::memcpy(mem->efa_add_ptr, local_ep_addrs, mem->efa_addr_size);
   };
 
   /* insert remote efa addr into address vector */
@@ -151,12 +174,12 @@ class SHMWorker {
     fi_av_insert(efa->av, i->data, 1, &(efa->peer_addr), 0, NULL);
     efa->av_ready = true;
 
-    char name_buf[efa_addr_size];
+    char name_buf[mem->efa_addr_size];
     char readable[64] = {0};
     size_t len = 64;
     fi_av_lookup(efa->av, efa->peer_addr, name_buf, &len);
     len = 64;
-    std::fill_n(readable, efa_addr_size, 0);
+    std::fill_n(readable, mem->efa_addr_size, 0);
     fi_av_straddr(efa->av, name_buf, readable, &len);
     std::cout << "verified inserted: " << readable << "\n";
   };
@@ -167,37 +190,37 @@ class SHMWorker {
       wait_cq(efa->txcq, 1);
       std::cout << name << ": efa send msg: " << i->data << "\n";
     } else {
-      fi_recv(efa->ep, data_buf_ptr, 64, NULL, FI_ADDR_UNSPEC, NULL);
+      fi_recv(efa->ep, mem->data_buf_ptr, 64, NULL, FI_ADDR_UNSPEC, NULL);
       wait_cq(efa->rxcq, 1);
-      std::cout << name << ": efa recv msg: " << data_buf_ptr << "\n";
+      std::cout << name << ": efa recv msg: " << mem->data_buf_ptr << "\n";
     }
-    (*(int*)cntr_ptr) += 1;
+    (*(int*)mem->cntr_ptr) += 1;
   };
 
   /* assume the worker stores the partition of the parameters
     currently, simplest 5MB for each batch with 200MB total
   */
   void efa_send_recv_param(EFAEndpoint* efa, Instruction* i, bool is_send) {
-    int total_size = 100 * 1024 * 1024; // 100 MB
-    int batch_p_size = 5 * 1024 * 1024; // 5 MB
+    int total_size = 100 * 1024 * 1024;  // 100 MB
+    int batch_p_size = 5 * 1024 * 1024;  // 5 MB
 
     if (is_send) {
       for (int i = 0; i < total_size / batch_p_size; ++i) {
-        char* _buf_s = ((char*)data_buf_ptr) + i * batch_p_size;
+        char* _buf_s = ((char*)mem->data_buf_ptr) + i * batch_p_size;
         fi_send(efa->ep, _buf_s, batch_p_size, NULL, efa->peer_addr, NULL);
       }
       // wait for transition queue
-      wait_cq(efa->txcq, total_size/batch_p_size);
+      wait_cq(efa->txcq, total_size / batch_p_size);
     } else {
-      for (int i = 0; i < total_size/batch_p_size; ++i) {
-        char* _buf_s = ((char*)data_buf_ptr) + i * batch_p_size;
+      for (int i = 0; i < total_size / batch_p_size; ++i) {
+        char* _buf_s = ((char*)mem->data_buf_ptr) + i * batch_p_size;
         fi_recv(efa->ep, _buf_s, batch_p_size, NULL, 0, NULL);
       }
       // wait for receive queue
       wait_cq(efa->rxcq, total_size / batch_p_size);
     }
     // increase the counter; later can modify to progressively increase
-    (*(int*)cntr_ptr) += total_size / batch_p_size;
+    (*(int*)mem->cntr_ptr) += total_size / batch_p_size;
   }
 
   // main function of the shm worker
@@ -206,7 +229,7 @@ class SHMWorker {
       Instruction* i = read_instr();
       if (i) {
         // set worker status as working
-        *(int*)status_ptr = 2;
+        *(int*)mem->status_ptr = 2;
         switch (i->type) {
           case SET_EFA_ADDR:
             this->set_remote_efa_addr(efa_ep, i);
@@ -225,7 +248,7 @@ class SHMWorker {
             break;
         }
         // change worker status
-        *(int*)status_ptr = 1;  // back to idle
+        *(int*)mem->status_ptr = 1;  // back to idle
         // clear instruction
         delete i;
       } else {
@@ -235,19 +258,8 @@ class SHMWorker {
   };
 
   ~SHMWorker() {
-    // un map
-    munmap(instr_ptr, instr_size);
-    munmap(cntr_ptr, cntr_size);
-    munmap(data_buf_ptr, data_buf_size);
-    munmap(efa_add_ptr, efa_addr_size);
-    munmap(status_ptr, status_size);
-
-    // un link
-    shm_unlink(shm_instr.c_str());
-    shm_unlink(shm_cntr.c_str());
-    shm_unlink(shm_data_buf.c_str());
-    shm_unlink(shm_efa_addr.c_str());
-    shm_unlink(shm_w_status.c_str());
+    delete mem;
+    delete efa_ep;
   }
 };
 };  // namespace trans
