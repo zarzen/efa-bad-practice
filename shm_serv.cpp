8bd043c091954e62f018ac4a314bf882381291ca
Zhen Zhang <zarzen@Zhens-iMac.local>
2020-03-17 17:30:59 -0400

    ready to compile

--- shm_serv.cpp
+++ shm_serv.cpp
@@ -7,70 +7,66 @@
 #include "util.h"
 
 using namespace trans;
-
-void cli_efa_addr_exchange(std::string& ip,
-                           std::string& port,
-                           std::vector<shm::WorkerMemory*>& workers) {
-  trans::SockCli cli(ip, port);
-  for (int i = 0; i < workers.size(); i++) {
-    char add_buf[64];
-    get_worker_efa_addr(workers[i], add_buf);
-    cli._send(add_buf, 64);
-  }
-
-  // recv remote EFA addrs
+void serv_efa_addr_exchange(std::string& ip,
+                            std::string& port,
+                            std::vector<shm::WorkerMemory*>& workers) {
+  trans::SockServ serv(port);
+  serv._listen();
+  // recv remote efa addrs
   for (int i = 0; i < workers.size(); i++) {
     char addr_buf[64];
-    cli._recv(addr_buf, 64);
-
-    // insert the address into worker
+    serv._recv(addr_buf, 64);
     set_peer_addr(workers[i], addr_buf);
+  }
+
+  // send local efa addrs
+  for (int i = 0; i < workers.size(); i++) {
+    char addr_buf[64];
+    get_worker_efa_addr(workers[i], addr_buf);
+    serv._send(addr_buf, 64);
   }
 };
 
-
-void put_efa_send_instr(shm::WorkerMemory* w) {
+void put_efa_recv_instr(shm::WorkerMemory* w) {
   w->mem_lock("efa send request, lock err");
-  // EFA send instr
-  *(int*)((char*)w->instr_ptr + 8) = shm::reverse_map(shm::SEND_INSTR);
-  std::string req_msg = "<fake-request-for-parameters>";
-  memcpy((char*)w->instr_ptr + 12, req_msg.c_str(), req_msg.length());
+  // recv fake instruction e.g. request for parameters of model xxx
+  *(int*)((char*)w->instr_ptr + 8) = shm::reverse_map(shm::RECV_INSTR);
   double ts = trans::time_now();
   *((double*)(w->instr_ptr)) = ts;
 
   w->mem_unlock("efa send request, unlock err");
 };
 
-void put_efa_recv_params(shm::WorkerMemory* w) {
-  w->mem_lock("efa recv params, lock err");
-  *(int*)((char*)w->instr_ptr + 8) = shm::reverse_map(shm::RECV_PARAM);
+void put_efa_send_params(shm::WorkerMemory* w) {
+  w->mem_lock("efa send params, lock err");
+  *(int*)((char*)w->instr_ptr + 8) = shm::reverse_map(shm::SEND_PARAM);
   *((double*)(w->instr_ptr)) = trans::time_now();
-  w->mem_unlock("efa recv params, unlock err");
-};
+  w->mem_unlock("efa send params, unlock err");
+}
 
-void fake_param_trans(std::vector<shm::WorkerMemory*>& workers) {
+void fake_serv_params(std::vector<shm::WorkerMemory*>& workers) {
   int cur_w = 0;
   int n_w = workers.size();
 
   while (get_worker_status(workers[cur_w]) != 1) {
     std::this_thread::sleep_for(std::chrono::microseconds(100));
   }
-
-  // set fake request
+  // recv fake request
   int cur_cntr = get_worker_cntr(workers[cur_w]);
-  put_efa_send_instr(workers[cur_w]);
-  // wait
+  put_efa_recv_instr(workers[cur_w]);
+  // wait for completion
   while (get_worker_status(workers[cur_w]) != 1 &&
          get_worker_cntr(workers[cur_w]) != cur_cntr + 1) {
     std::this_thread::sleep_for(std::chrono::microseconds(100));
   }
+
   double st = trans::time_now();
-  // start receive
+  // start send parameters
   for (int i = 0; i < n_w; i++) {
-    shm::WorkerMemory* w = workers[i];
-    put_efa_recv_params(w);
+    put_efa_send_params(workers[i]);
   }
 
+  // wait for job completion
   while (1) {
     bool all_done = true;
     for (int i = 0; i < n_w; i++) {
@@ -84,14 +80,15 @@ void fake_param_trans(std::vector<shm::WorkerMemory*>& workers) {
     else
       std::this_thread::sleep_for(std::chrono::microseconds(100));
   }
+
   double et = trans::time_now();
   double bw = (200 * 1024 * 1024 * 8 / (et - st)) / 1e9;
-  std::cout << "Recv params bw: " << bw << " Gbps\n";
-};
+  std::cout << "Send params bw: " << bw << " Gbps\n";
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 3) {
-    std::cerr << "Usage: ./shm_cli <ip> <port>";
+    std::cerr << "Usage: ./shm_serv <ip> <port>";
   }
   std::string ip(argv[1]);
   std::string port(argv[2]);
@@ -108,10 +105,11 @@ int main(int argc, char* argv[]) {
     worker_names.push_back(name);
     sharedWorkers.push_back(new shm::WorkerMemory(name, false));
   }
-  cli_efa_addr_exchange(ip, port, sharedWorkers);
+
+  serv_efa_addr_exchange(ip, port, sharedWorkers);
   // make sure addrs inserted
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
-  fake_param_trans(sharedWorkers);
-  // delete workers memory
+  //
+  fake_serv_params(sharedWorkers);
 }
