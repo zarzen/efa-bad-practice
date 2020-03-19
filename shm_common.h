c88a95222bdd7179dccc5d3bfe348b5a308590ed
Zhen Zhang <zarzen@Zhens-iMac.local>
2020-03-19 11:49:12 -0400

    update worker memory and worker

--- shm_common.h
+++ shm_common.h
@@ -25,7 +25,6 @@ void check_err(bool cond, std::string msg) {
 namespace trans {
 namespace shm {
 
-
 enum INSTR_T {
   ERR_INSTR,
   SET_EFA_ADDR,
@@ -39,7 +38,7 @@ class Instruction {
  public:
   INSTR_T type;
   double timestamp;
-  char* data;
+  char data[500] = {0};
   Instruction(){};
 };
 
@@ -79,13 +78,16 @@ int reverse_map(INSTR_T t) {
 
 class WorkerMemory {
  public:
-  // 12 + 64 Bytes: 8 bytes for timestamp;
-  // 4 bytes for instr-code; 64 bytes for instr data
-  int instr_size = 76;
+  // 12 + 500 Bytes:
+  // 8 bytes for timestamp;
+  // 4 bytes for instr-code; the remaining bytes for instr data
+  int instr_size = 512;
   int efa_addr_size = 64;
-  int cntr_size = 4;                      // 4 Bytes
-  int data_buf_size = 500 * 1024 * 1024;  // 500 MB
-  int status_size = 4;                    // indicate the status of worker
+  int cntr_size = 4;                     // 4 Bytes
+  int status_size = 4;                   // indicate the status of worker
+  unsigned long long int data_buf_size;  // input from user
+  // based on rank to move the pointer of instr, efa_addr, cntr, status
+  int rank;
 
   // shm identifiers
   std::string shm_instr;
@@ -93,77 +95,107 @@ class WorkerMemory {
   std::string shm_data_buf;
   std::string shm_efa_addr;
   std::string shm_w_status;
-  std::string shm_mutex_name;
 
+  // mutex names
+  std::string sem_name_instr;
+  std::string sem_name_cntr;
+  std::string sem_name_data;
+  std::string sem_name_efa_addr;
+  std::string sem_name_w_status;
+
+  // mutex pointers
+  sem_t* sem_instr;
+  sem_t* sem_cntr;
+  sem_t* sem_data;
+  sem_t* sem_efa_addr;
+  sem_t* sem_w_status;
+
+  // pointers
   void* instr_ptr;
   void* cntr_ptr;
   void* data_buf_ptr;
   void* efa_add_ptr;
   void* status_ptr;  // 1: idle; 2: working;
-  sem_t* sem_mutex;
 
-  WorkerMemory(std::string prefix, bool truncate) {
-    shm_instr = std::string(prefix + "-instr-mem");
-    shm_cntr = std::string(prefix + "-cntr-mem");
-    shm_data_buf = std::string(prefix + "-data-buf-mem");
-    shm_efa_addr = std::string(prefix + "-efa-addr-mem");
-    shm_w_status = std::string(prefix + "-worker-status-mem");
-    shm_mutex_name = std::string("/" + prefix + "-mem-mutex");
-    this->create_shm(truncate);
-    // create lock
-    sem_mutex = sem_open(shm_mutex_name.c_str(), O_CREAT, S_IRUSR | S_IWUSR, 1);
-    this->print_sem_mutex_val();
+  /* rank start from 0
+   */
+  WorkerMemory(std::string prefix, int rank, unsigned long long int data_size) {
+    data_buf_size = data_size;
+    this->rank = rank;
+
+    shm_instr = std::string(prefix + "-instr-mem");        // instruction
+    shm_cntr = std::string(prefix + "-cntr-mem");          // counter
+    shm_efa_addr = std::string(prefix + "-efa-addr-mem");  // local efa addr
+    shm_w_status = std::string(prefix + "-worker-status-mem");  // worker status
+    shm_data_buf = std::string(prefix + "-data-buf-mem");       // data buf
+
+    sem_name_instr =
+        std::string("/" + prefix + "-instr-mtx-" + std::to_string(rank));
+    sem_name_cntr =
+        std::string("/" + prefix + "-cntr-mtx-" + std::to_string(rank));
+    sem_name_data =
+        std::string("/" + prefix + "-data-buf-mtx-" + std::to_string(rank));
+    sem_name_efa_addr =
+        std::string("/" + prefix + "-efa-addr-mtx-" + std::to_string(rank));
+    sem_name_w_status = std::string("/" + prefix + "-worker-status-mtx-" +
+                                    std::to_string(rank));
+
+    this->open_shm_sem();
   };
 
-  void create_shm(bool truncate) {
-    int instr_fd = shm_open(shm_instr.c_str(), O_CREAT | O_RDWR, 0666);
-    int cntr_fd = shm_open(shm_cntr.c_str(), O_CREAT | O_RDWR, 0666);
-    int data_buf_fd = shm_open(shm_data_buf.c_str(), O_CREAT | O_RDWR, 0666);
-    int efa_add_fd = shm_open(shm_efa_addr.c_str(), O_CREAT | O_RDWR, 0666);
-    int worker_status_fd =
-        shm_open(shm_w_status.c_str(), O_CREAT | O_RDWR, 0666);
-
-    // truncate memory size
-    if (truncate) {
-      check_err((ftruncate(instr_fd, instr_size) < 0),
-                "ftruncate instr_fd err\n");
-      check_err((ftruncate(cntr_fd, cntr_size) < 0), "ftruncate cntr_fd err\n");
-      check_err((ftruncate(data_buf_fd, data_buf_size) < 0),
-                "ftruncate data_buf_fd err\n");
-      check_err((ftruncate(efa_add_fd, efa_addr_size) < 0),
-                "ftruncate efa_add_fd err\n");
-      check_err((ftruncate(worker_status_fd, status_size) < 0),
-                "ftruncate worker_status_fd err\n");
-    }
+  void open_shm_sem() {
+    // only open shm not create
+    // creation is handled by controller side
+    int instr_fd = shm_open(shm_instr.c_str(), O_RDWR, 0666);
+    int cntr_fd = shm_open(shm_cntr.c_str(), O_RDWR, 0666);
+    int data_buf_fd = shm_open(shm_data_buf.c_str(), O_RDWR, 0666);
+    int efa_add_fd = shm_open(shm_efa_addr.c_str(), O_RDWR, 0666);
+    int worker_status_fd = shm_open(shm_w_status.c_str(), O_RDWR, 0666);
+    // map memory pointer
     instr_ptr =
         mmap(0, instr_size, PROT_READ | PROT_WRITE, MAP_SHARED, instr_fd, 0);
     cntr_ptr =
         mmap(0, cntr_size, PROT_READ | PROT_WRITE, MAP_SHARED, cntr_fd, 0);
-    data_buf_ptr = mmap(0, data_buf_size, PROT_READ | PROT_WRITE, MAP_SHARED,
-                        data_buf_fd, 0);
     efa_add_ptr = mmap(0, efa_addr_size, PROT_READ | PROT_WRITE, MAP_SHARED,
                        efa_add_fd, 0);
     status_ptr = mmap(0, status_size, PROT_READ | PROT_WRITE, MAP_SHARED,
                       worker_status_fd, 0);
+    data_buf_ptr = mmap(0, data_buf_size, PROT_READ | PROT_WRITE, MAP_SHARED,
+                        data_buf_fd, 0);
+    // move the pointer
+    instr_ptr = (void*)((char*)instr_ptr + this->rank * instr_size);
+    cntr_ptr = (void*)((char*)cntr_ptr + this->rank * cntr_size);
+    efa_add_ptr = (void*)((char*)efa_add_ptr + this->rank * efa_addr_size);
+    status_ptr = (void*)((char*)status_ptr + this->rank * status_size);
+
+    // open mutexs
+    // controller will create the semaphore
+    // sem_instr = sem_open(sem_name_instr.c_str(), O_CREAT, S_IRUSR | S_IWUSR,
+    // 1);
+    sem_instr = sem_open(sem_name_instr.c_str(), 0);
+    sem_cntr = sem_open(sem_name_cntr.c_str(), 0);
+    sem_data = sem_open(sem_name_data.c_str(), 0);
+    sem_efa_addr = sem_open(sem_name_efa_addr.c_str(), 0);
+    sem_w_status = sem_open(sem_name_w_status.c_str(), 0);
   };
 
-  void mem_lock(std::string msg_if_err) {
-    check_err(sem_wait(sem_mutex) < 0, msg_if_err);
+  void mem_lock(sem_t* s, std::string msg_if_err) {
+    check_err(sem_wait(s) < 0, msg_if_err);
   };
 
-  void mem_unlock(std::string msg_if_err) {
-    check_err(sem_post(sem_mutex) < 0, msg_if_err);
+  void mem_unlock(sem_t* s, std::string msg_if_err) {
+    check_err(sem_post(s) < 0, msg_if_err);
   };
 
-  void print_sem_mutex_val(){
+  void print_sem_mutex_val(sem_t* s) {
     int v;
-    sem_getvalue(sem_mutex, &v);
+    sem_getvalue(s, &v);
     std::cout << "sem_mutex val: " << v << "\n";
   };
 
-  int sem_mutex_val() {
+  int sem_mutex_val(sem_t* s) {
     int v;
-    sem_getvalue(sem_mutex, &v);
+    sem_getvalue(s, &v);
     return v;
   };
 
@@ -181,8 +213,13 @@ class WorkerMemory {
     shm_unlink(shm_data_buf.c_str());
     shm_unlink(shm_efa_addr.c_str());
     shm_unlink(shm_w_status.c_str());
-    // un link sem
-    sem_unlink(shm_mutex_name.c_str());
+
+    // un link semaphore
+    sem_unlink(sem_name_instr.c_str());
+    sem_unlink(sem_name_cntr.c_str());
+    sem_unlink(sem_name_data.c_str());
+    sem_unlink(sem_name_efa_addr.c_str());
+    sem_unlink(sem_name_w_status.c_str());
   };
 };
 
@@ -192,9 +229,11 @@ class SHMWorker {
   WorkerMemory* mem;
 
  public:
-  SHMWorker(std::string name) {
+  SHMWorker(std::string name,
+            int rank,
+            unsigned long long int shared_data_size) {
     efa_ep = new EFAEndpoint(this->name + "-efa-ep");
-    mem = new WorkerMemory(name, true);
+    mem = new WorkerMemory(name, rank, shared_data_size);
     this->name = name;
     this->set_local_efa_addr(efa_ep);
     // init worker status
@@ -202,7 +241,7 @@ class SHMWorker {
   };
 
   Instruction* read_instr() {
-    mem->mem_lock("read_instr: sem_wait err");
+    mem->mem_lock(mem->sem_instr, "read_instr: sem_wait err");
     // read first 8 bytes for time stamp
     double ts = *((double*)mem->instr_ptr);
     Instruction* i = NULL;
@@ -213,11 +252,11 @@ class SHMWorker {
       // start from 9th byte to 12th stand for instr
       INSTR_T i_type = instr_map(*((int*)((char*)mem->instr_ptr + 8)));
       i->type = i_type;
-      i->data = ((char*)mem->instr_ptr) + 12;
+      memcpy(i->data, ((char*)mem->instr_ptr) + 12, 500);
       // wipe first 12 bytes to inidicate message already read
       std::fill_n((char*)mem->instr_ptr, 12, 0);
     }
-    mem->mem_unlock("read_instr: sem_post err");
+    mem->mem_unlock(mem->sem_instr, "read_instr: sem_post err");
     // mem->print_sem_mutex_val();
     return i;
   };
@@ -230,17 +269,16 @@ class SHMWorker {
     fi_av_straddr(efa->av, local_ep_addrs, readable, &len);
     std::cout << "Local ep addresses: \n" << readable << "\n";
 
-    mem->mem_lock("set_local_efa_addr: sem_wait err");
+    mem->mem_lock(mem->sem_efa_addr, "set_local_efa_addr: sem_wait err");
     std::memcpy(mem->efa_add_ptr, local_ep_addrs, mem->efa_addr_size);
-    mem->mem_unlock("set_local_efa_addr: sem_post err");
-    mem->print_sem_mutex_val();
+    mem->mem_unlock(mem->sem_efa_addr, "set_local_efa_addr: sem_post err");
+    mem->print_sem_mutex_val(mem->sem_efa_addr);
   };
 
   /* insert remote efa addr into address vector */
   void set_remote_efa_addr(EFAEndpoint* efa, Instruction* i) {
-    mem->mem_lock("set_remote_efa_addr: sem_wait err");
+    // instruction data already copied into i->data
     fi_av_insert(efa->av, i->data, 1, &(efa->peer_addr), 0, NULL);
-    mem->mem_unlock("set_remote_efa_addr: sem_post err");
 
     efa->av_ready = true;
 
@@ -267,9 +305,11 @@ class SHMWorker {
       std::cout << name << ": efa recv msg: " << _msg << "\n";
     }
 
-    mem->mem_lock("efa_send_recv_instr: increase cntr sem_wait err");
+    mem->mem_lock(mem->sem_cntr,
+                  "efa_send_recv_instr: increase cntr sem_wait err");
     (*(int*)mem->cntr_ptr) += 1;
-    mem->mem_unlock("efa_send_recv_instr: increase cntr sem_post err");
+    mem->mem_unlock(mem->sem_cntr,
+                    "efa_send_recv_instr: increase cntr sem_post err");
   };
 
   /* assume the worker stores the partition of the parameters
@@ -296,10 +336,12 @@ class SHMWorker {
     }
     std::cout << "send/recv params cost: " << time_now() - _st << "\n";
 
-    mem->mem_lock("efa_send_recv_param: increase cntr sem_wait err");
+    mem->mem_lock(mem->sem_cntr,
+                  "efa_send_recv_param: increase cntr sem_wait err");
     // increase the counter; later can modify to progressively increase
     (*(int*)mem->cntr_ptr) += total_size / batch_p_size;
-    mem->mem_unlock("efa_send_recv_param: increase cntr sem_post err");
+    mem->mem_unlock(mem->sem_cntr,
+                    "efa_send_recv_param: increase cntr sem_post err");
   }
 
   // main function of the shm worker
@@ -308,10 +350,10 @@ class SHMWorker {
       Instruction* i = read_instr();
       if (i) {
         std::cout << "instr type " << i->type << "\n";
-        mem->mem_lock("set worker status: sem_wait err");
+        mem->mem_lock(mem->sem_w_status, "set worker status: sem_wait err");
         // set worker status as working
         *(int*)mem->status_ptr = 2;
-        mem->mem_unlock("set worker status: sem_post err");
+        mem->mem_unlock(mem->sem_w_status, "set worker status: sem_post err");
         switch (i->type) {
           case SET_EFA_ADDR:
             std::cout << "task for set efa addr\n";
@@ -337,10 +379,10 @@ class SHMWorker {
             std::cerr << "err instr encountered \n";
             break;
         }
-        mem->mem_lock("set worker status: sem_wait err");
+        mem->mem_lock(mem->sem_w_status, "set worker status: sem_wait err");
         // change worker status
         *(int*)mem->status_ptr = 1;  // back to idle
-        mem->mem_unlock("set worker status: sem_post err");
+        mem->mem_unlock(mem->sem_w_status, "set worker status: sem_post err");
         // clear instruction
         delete i;
       } else {
@@ -356,33 +398,33 @@ class SHMWorker {
 };
 
 void get_worker_efa_addr(WorkerMemory* shm_w, char* addr_buf) {
-  shm_w->mem_lock("lock at get_worker_efa_addr, err");
+  shm_w->mem_lock(shm_w->sem_efa_addr, "lock at get_worker_efa_addr, err");
   memcpy(addr_buf, shm_w->efa_add_ptr, shm_w->efa_addr_size);
-  shm_w->mem_unlock("unlock at get_worker_efa_addr, err");
+  shm_w->mem_unlock(shm_w->sem_efa_addr, "unlock at get_worker_efa_addr, err");
 };
 
 void set_peer_addr(WorkerMemory* shm_w, char* addr_buf) {
-  shm_w->mem_lock("insert remote efa addr lock err");
+  shm_w->mem_lock(shm_w->sem_instr, "insert remote efa addr lock err");
   *(int*)((char*)shm_w->instr_ptr + 8) =
       trans::shm::reverse_map(trans::shm::SET_EFA_ADDR);  // SET EFA ADDR
   memcpy((char*)shm_w->instr_ptr + 12, addr_buf, 64);
   // set timestamp at last,
   double timestamp = trans::time_now();
   *((double*)(shm_w->instr_ptr)) = timestamp;
-  shm_w->mem_unlock("insert remote efa addr unlock err");
+  shm_w->mem_unlock(shm_w->sem_instr, "insert remote efa addr unlock err");
 }
 
-int get_worker_status(WorkerMemory* w) {
-  w->mem_lock("lock while getting worker status");
-  int s = *(int*)(w->status_ptr);
-  w->mem_unlock("unlock while getting worker status");
+int get_worker_status(WorkerMemory* wm) {
+  wm->mem_lock(wm->sem_w_status, "lock while getting worker status");
+  int s = *(int*)(wm->status_ptr);
+  wm->mem_unlock(wm->sem_w_status, "unlock while getting worker status");
   return s;
 };
 
-int get_worker_cntr(WorkerMemory* w) {
-  w->mem_lock("getting cntr, lock err");
-  int c = *(int*)w->cntr_ptr;
-  w->mem_unlock("getting cntr, unlock err");
+int get_worker_cntr(WorkerMemory* wm) {
+  wm->mem_lock(wm->sem_cntr, "getting cntr, lock err");
+  int c = *(int*)wm->cntr_ptr;
+  wm->mem_unlock(wm->sem_cntr, "getting cntr, unlock err");
   return c;
 };
 
