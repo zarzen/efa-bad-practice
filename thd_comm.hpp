3aff0607e661f751b28173cd1e322b782f5f8285
Zhen Zhang <zhangzhen.dev@gmail.com>
2020-04-10 00:48:28 -0400

    WIP

--- thd_comm.hpp
+++ thd_comm.hpp
@@ -1,21 +1,28 @@
 #ifndef THD_COMM_H
 #define THD_COMM_H
 
-#include "spdlog/spdlog.h"
-#include "util.h"
 #include <iostream>
 #include <memory>
+#include "sock_cli_serv.h"
+#include "spdlog/spdlog.h"
+#include "thd_efa_worker.hpp"
+#include "util.h"
 
 namespace pipeps {
 
-namespace trans {
-
-enum MsgType { EFA_ADDR_INFO, SEND_ONE, SEND_BATCH, RECV_ONE, RECV_BATCH };
+enum MsgType {
+  EFA_ADDR_INFO,
+  SEND_ONE,
+  SEND_BATCH,
+  RECV_ONE,
+  RECV_BATCH,
+  SHUTDOWN
+};
 
 class TransMsg {
-public:
+ public:
   MsgType t;
-  char *data;
+  char* data;
   size_t len;
   TransMsg(MsgType _t, size_t data_len) {
     t = _t;
@@ -23,14 +30,16 @@ public:
     len = data_len;
   };
 
-  TransMsg(const TransMsg &obj) {
+  TransMsg(){};
+
+  TransMsg(const TransMsg& obj) {
     this->data = new char[obj.len];
     this->len = obj.len;
     this->t = obj.t;
     memcpy(this->data, obj.data, len);
   }
 
-  TransMsg(TransMsg &&obj) {
+  TransMsg(TransMsg&& obj) {
     t = obj.t;
     data = obj.data;
     len = obj.len;
@@ -38,33 +47,49 @@ public:
     obj.data = nullptr;
   };
 
+  TransMsg& operator=(const TransMsg& other) {
+    std::cout << "customized assignment op \n";
+    this->t = other.t;
+    this->data = other.data;
+    this->len = other.len;
+    return *this;
+  };
+
   ~TransMsg() { delete[] data; }
 };
 
 class ThdCommunicator {
-public:
+ public:
   const static int efaAddrSize{64};
   // for workers
   std::vector<std::thread> workerThds;
-  std::vector<ThdSafeQueue<TransMsg> *> workerTaskQs;
-  std::vector<std::atomic<size_t> *> workerCntrs;
-  std::atomic<int> addrReadyC{0};
+  std::vector<ThdSafeQueue<TransMsg>*> workerTaskQs;
+  std::vector<std::atomic<size_t>*> workerCntrs;
+  std::atomic<int>* addrReadyC;
   // potential usage for worker to report msg to communicator
   // std::vector<ThdSafeQueue<TransMsg>*> workerMsgQs;
 
   // communicator vars
+  std::string name;
+  int nw;
+  std::string listenPort;
   std::string dstIP;
   std::string dstPort;
-  bool ready{false}; // peer EFA addrs is not ready at first
+  bool _ready{false};  // peer EFA addrs is not ready at first
   char* efaAddrs;
-  std::atomic<bool> cntr;
+  std::atomic<size_t> cntr;
+  std::atomic<bool> exit;
+  std::thread* sockThdPtr;
+  std::thread* cntrThdPtr;
 
-  ThdCommunicator(std::string listenPort, std::string dstIP,
-                  std::string dstPort, int nw);
+  ThdCommunicator(std::string listenPort,
+                  std::string dstIP,
+                  std::string dstPort,
+                  int nw);
   ~ThdCommunicator();
 
-  void asend(char *buf, size_t len);
-  void arecv(char *buf, size_t len);
+  void asend(char* buf, size_t len);
+  void arecv(char* buf, size_t len);
 
   // will be invoked at the first time asend/arecv is called
   // it is a block function will retry several times
@@ -72,33 +97,123 @@ public:
   bool getPeerAddrs();
 
   // always listening for others to query
-  static void socketListener(std::string port, char *addrsBuf, size_t addrsLen);
+  static void socketListenerThdFun(ThdCommunicator* comm,
+                                   std::string port,
+                                   char* addrsBuf,
+                                   size_t addrsLen);
 
-  static void workerThdFun(std::string workerName, int rank,
-                           ThdSafeQueue<TransMsg>* taskq, std::atomic<size_t>* cntr,
-                            char *efaAddrs);
+  static void cntrMonitorThdFun(ThdCommunicator* comm);
 };
 
-ThdCommunicator::ThdCommunicator(std::string listenPort, std::string dstIP,
-                                 std::string dstPort, int nw) {
-                                   efaAddrs = new char[nw * efaAddrSize];
-                                   // start workers
-                                   // make sure EFA addr ready via addrReadyC
-                                   // launch socket listener
-                                 };
+ThdCommunicator::ThdCommunicator(std::string listenPort,
+                                 std::string dstIP,
+                                 std::string dstPort,
+                                 int nw) {
+  this->nw = nw;
+  this->listenPort = listenPort;
+  this->dstIP = dstIP;
+  this->dstPort = dstPort;
+  this->name = listenPort + "comm";
 
-// send msg to workers to stop
+  efaAddrs = new char[nw * efaAddrSize];
+  addrReadyC = new std::atomic<int>(0);
+  // start workers
+  for (int i = 0; i < nw; i++) {
+    std::string _wn = this->name + "-worker-" + std::to_string(i);
+    ThdSafeQueue<TransMsg>* _wtq = new ThdSafeQueue<TransMsg>();
+    std::atomic<size_t>* _wc = new std::atomic<size_t>();
+    std::thread _wt(efaWorkerThdFun, _wn, i, _wtq, _wc, efaAddrs, addrReadyC);
+    workerThds.push_back(std::move(_wt));
+
+    spdlog::debug("{:s} started worker {:s} in thread", this->name, _wn);
+  }
+  // make sure EFA addr ready via addrReadyC
+  while (*addrReadyC < nw) {
+    std::this_thread::sleep_for(std::chrono::microseconds(100));
+  }
+  // launch socket listener
+  sockThdPtr = new std::thread(socketListenerThdFun, this, listenPort, efaAddrs,
+                               nw * efaAddrSize);
+  spdlog::debug("EFA address server started");
+  // launch cntr
+  cntrThdPtr = new std::thread(cntrMonitorThdFun, this);
+  spdlog::debug("Communicator cntr update thread started");
+};
+
 // set the communicator status to stop for socket thread to quit
+// send msg to workers to stop
 // wait for worker threads to join
 // wait for socket thread to stop
 // clean resource
-ThdCommunicator::~ThdCommunicator(){
-  //
+ThdCommunicator::~ThdCommunicator() {
+  // 0: ===========
+  this->exit = true;
+  // 0: ===========
 
-  // 
+  // 1: ======= hack to stop the socketListener
+  trans::SockCli cli("127.0.0.1", listenPort);
+  std::unique_ptr<char> _t(new char[nw * efaAddrSize]);
+  cli._recv(_t.get(), nw * efaAddrSize);
+  sockThdPtr->join();
+  // 1: ======= end stopping socket Listener
+
+  // 2: =======
+  for (int i = 0; i < nw; i++) {
+    TransMsg _stop_msg(SHUTDOWN, 1);
+    workerTaskQs[i]->push(std::move(_stop_msg));
+    workerThds[i].join();
+  }
+  // 2: =======
+
+  // 3: stop wait cntr thd
+  cntrThdPtr->join();
+  // 3: end
+
+  // clean resources
   delete[] efaAddrs;
+  delete cntrThdPtr;
+  delete sockThdPtr;
+  for (int i = 0; i < nw; i++) {
+    delete workerCntrs[i];
+    delete workerTaskQs[i];
+  }
+  delete addrReadyC;
 };
-}; // namespace trans
-}; // namespace pipeps
+
+// only used for
+void ThdCommunicator::socketListenerThdFun(ThdCommunicator* comm,
+                                           std::string port,
+                                           char* addrsBuf,
+                                           size_t addrsLen) {
+  trans::SockServ serv(port);
+  while (!comm->exit) {
+    // quick hack to stop this thread:
+    // set the comm->exit = true;
+    // then connect to this sock and recv
+    int cli_fd = serv._listen();  // here will block;
+    size_t ret = send(cli_fd, addrsBuf, addrsLen, 0);
+    if (ret == -1) {
+      spdlog::critical("Err, while sending out EFA addrs");
+    }
+  }
+};
+
+void ThdCommunicator::cntrMonitorThdFun(ThdCommunicator* comm) {
+  while (!comm->exit) {
+    bool inc = true;
+    // this is bounded because of the task splitting
+    // check whether all worker cntrs greater than current communicator cntr
+    for (auto c_ptr : comm->workerCntrs) {
+      if (*c_ptr <= comm->cntr) {
+        inc = false;
+      }
+    }
+    if (inc) {
+      comm->cntr += 1;
+    }
+  }
+};
+
+};  // namespace pipeps
 
 #endif
---
+++ thd_efa_worker.hpp
@@ -0,0 +1,61 @@
+#ifndef THD_EFA_WORKER_H
+#define THD_EFA_WORKER_H
+
+#include "efa_ep.h"
+#include "thd_comm.hpp"
+
+namespace pipeps {
+
+void workerWaitCq(std::string& workerName, fid_cq* cq, int count) {
+  struct fi_cq_err_entry entry;
+  int ret, completed = 0;
+  double s = time_now();
+  while (completed < count) {
+    ret = fi_cq_read(cq, &entry, 1);
+    if (ret == -FI_EAGAIN) {
+      continue;
+    }
+
+    if (ret == -FI_EAVAIL) {
+      spdlog::error("Error while checking completion");
+      ret = fi_cq_readerr(cq, &entry, 1);
+      char _err_buf[100];
+      fi_cq_strerror(cq, entry.prov_errno, entry.err_data, _err_buf, 100);
+      spdlog::error(
+          "Error while calling fi_cq_readerr, err code {:d}, err msg {:s}", ret,
+          _err_buf);
+    }
+
+    if (ret < 0)
+      spdlog::error("{:s} fi_cq_read err", workerName);
+    completed++;
+
+    double cost_t = time_now() - s;
+    spdlog::debug("{:s} completes {:d} job cost: {:lf} ms", workerName,
+                  completed, cost_t * 1e3);
+    s = time_now();  // update start time
+  }
+};
+
+void workerConvertMsg(TransMsg& msg,
+                      std::vector<std::pair<void*, size_t>>& ptrs);
+
+void efaWorkerThdFun(std::string workerName,
+                     int rank,
+                     ThdSafeQueue<TransMsg>* taskq,
+                     std::atomic<size_t>* cntr,
+                     char* efaAddrs,
+                     std::atomic<int>* addrReady) {
+  trans::EFAEndpoint efa_ep(workerName + "-efa-ep");
+  while (true) {
+    TransMsg* msg = new TransMsg();
+    taskq->pop(msg.get());
+
+
+    // clear
+    delete msg;
+  }
+};
+};  // namespace pipeps
+
+#endif
