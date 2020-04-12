fce7e9f4cb375d00fdae1feb097b5dc3614d998b
Zhen Zhang <zhangzhen.dev@gmail.com>
2020-04-12 03:16:32 +0000

    delete sleep operation

--- thd_comm.hpp
+++ thd_comm.hpp
@@ -180,7 +180,7 @@ ThdCommunicator::ThdCommunicator(std::string listenPort,
     workerCntrs.push_back(_wc);
     workerTaskQs.push_back(_wtq);
     spdlog::info("{:s} started worker {:s} in thread", this->name, _wn);
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    // std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
   // make sure EFA addr ready via addrReadyC
   while (*addrReadyC < nw) {
